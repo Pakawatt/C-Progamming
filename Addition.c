18a3605e78ef0e19a67765b05875893bb591c9ef
Pakawatt <s6302041620109@email.kmutnb.ac.th>
2021-01-12 15:48:48 +0700

    first commit

---
+++ Addition.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// ดึง Library Input output
+
+int main()
+{ // ส่วนที่ 2 Function หลักของภาษา C คือ Fx Main
+  // ------------------ คำสั่งโปรแกรมบวกเลข ------------------
+  int num1,num2,total;
+    //ประกาศตัวแปร
+
+    printf("Value of num1 is :");
+    scanf("%d",&num1);
+
+    printf("Value of num2 is :");
+    scanf("%d",&num2);
+
+    total = num1+ num2 ;
+    printf("%d",total);
+
+    
+    
+}
---
+++ Helloworld.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//preprocessing
+// ส่วนหัวสำหรับดึง libraly(1)
+
+//ส่วน function หลัก(2)
+int main()
+{
+    printf("NAME : Pakawat\n"); //ชื่อ
+    printf("Nickname : Pooh\n");//ชื่อเล่น
+    printf("Age : 18 years\n");//อายุ
+    printf("Favorite things : Sleep all day\n"); //สิ่งที่ชอบ
+    printf("What i want to do : Sleep Now!!!\n"); //สิ่งที่อยากทำ
+    printf("My happiness is sleeping\n"); //ความสุขของผม
+    /* 
+        ส่วนคำสั่ง(3)
+        printf คือฟังชั่นที่ไว้สำหรับแสดงผลหน้าจอ
+    */
+
+    // วิธีการจัดชุดคำสั่งให้สวยงาม shift + alt + f
+    // ctrl + j
+}
